6a38ed4d47b121621d88dd1aac41b62f13a345a6
Yuriy Bondar <bondar.europe@gmail.com>
2019-12-08 20:40:07 +0200

    adds home ang news-article page

---
+++ src/js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  /* menu */
+
+  $('.js-menu').on('click', function() {
+    $(this).toggleClass('active');
+    $('.menu-content').toggleClass('menu-open');
+    $('.menu-backdrop').toggleClass('menu-backdrop-open');
+  });
+
+  /* carousel */
+
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 2200,
+    autoplayHoverPause: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+    },
+  });
+});
